9c99ea056c9cebe1ce430bb2970de6572f1ea672
mimi! <72160950+tomatoseandcrying@users.noreply.github.com>
2025-04-13 19:30:56 -0400

    Moriah and Mary refactored

--- IsaacJokers.lua
+++ IsaacJokers.lua
@@ -1,11 +1,14 @@
 --[[ G.farmerProb = G.GAME.probabilities ]]
 
+--Visual Libraries
 SMODS.Atlas{
     key = 'IsaacJokers',
     path = "IsaacJokers.png",
     px = 71,
     py = 95
 }
+
+--Audio Libraries
 SMODS.Sound({
     key = "hpfx_1up",
     path = "1up.ogg",
@@ -19,6 +22,62 @@ SMODS.Sound({
     path = "gulp.ogg",
 })
 
+--General Refactor Functions
+local function chipScoring(card, context)
+    return{
+        chip_mod = card.ability.extra.chips,
+        sound = "hpfx_thumbsup",
+        colour = G.C.CHIPS,
+        message = localize{type = 'variable', key = 'a_chips', vars = {card.ability.extra.chips}}
+    }
+    
+end
+local function multScoring(card, context)
+    return{
+        mult_mod = card.ability.extra.mult,
+        sound = 'hpfx_1up',
+        message = localize{type = 'variable', key = 'a_mult', vars = {card.ability.extra.mult}}
+    }    
+end
+local function counterIncrement(card, context)
+    if context.main_eval and (to_big{card.ability.extra.mult, card.ability.extra.mult_gain} > to_big(1)) then
+        card.ability.extra.c_rounds = card.ability.extra.c_rounds + 1 --otherwise adds 1 to the counter
+    end
+end
+
+--Character-Specific Refactor Functions
+local function moriahMain(card, context)
+    if context.joker_main and (to_big(card.ability.extra.chips) > to_big(1)) and (to_big(card.ability.extra.mult) > to_big(1)) then --gives chips
+        return{
+            chipScoring(card, context), --chips scoring function
+            extra = {
+            multScoring(card, context), --mult scoring function
+            },
+        }
+    end
+    
+end
+local function maryMain(card, context)
+    if context.joker_main and G.GAME.current_round.hands_left == 0 and (to_big(card.ability.extra.mult) > to_big(1)) then --gives mult
+        multScoring(card, context) --mult scoring function
+    end
+end
+local function maryEnd(card, context)
+    if context.end_of_round then --triggers at end of round
+        counterIncrement(card, context) --increments the counter
+        if not context.blueprint_card and card.ability.extra.c_rounds >= card.ability.extra.rounds then  -- gains mult only if the required amount of rounds have passed
+            card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.mult_gain
+            card.ability.extra.c_rounds = 0 --resets counter
+            return { --custom message (for flavor <3)
+                message = 'Yum!',
+                sound = "hpfx_gulp",
+                colour = G.C.MULT
+            }
+        end
+    end
+end
+
+--Jokers
 SMODS.Joker{ --Moriah/Isaac
     key = 'moriah',
     config = {extra = {chips = 22, mult = 2, money = 1}},
@@ -39,24 +98,9 @@ SMODS.Joker{ --Moriah/Isaac
     eternal_compat = true,
     perishable_compat = true,
     calculate = function(self, card, context)
-        if context.joker_main and (to_big(card.ability.extra.chips) > to_big(1)) and (to_big(card.ability.extra.mult) > to_big(1)) then --Gives chips and mult
-            return{
-                chip_mod = card.ability.extra.chips,
-                sound = "hpfx_thumbsup",
-                colour = G.C.CHIPS,
-                message = localize{type = 'variable', key = 'a_chips', vars = {card.ability.extra.chips}},
-                extra = {
-                    mult_mod = card.ability.extra.mult,
-                    sound = "hpfx_1up",
-                    colour = G.C.MULT,
-                    message = localize{type = 'variable', key = 'a_mult', vars = {card.ability.extra.mult}},
-                },
-            card = card
-            }
-        end
+        return moriahMain(card, context)
     end
 }
-
 SMODS.Joker{ --Mary/Magdalene
     key = 'mary',
     config = {extra = {mult = 8, mult_gain = 2, rounds = 3, c_rounds = 0}},
@@ -69,37 +113,13 @@ SMODS.Joker{ --Mary/Magdalene
     blueprint_compat = true,
     eternal_compat = true,
     perishable_compat = true,
-    loc_vars = function (self, info_queue, card)
+    loc_vars = function (self, info_queue, card)    
         return{vars = {card.ability.extra.mult, card.ability.extra.mult_gain, card.ability.extra.rounds, card.ability.extra.c_rounds}}
     end,
     calculate = function (self, card, context)
-        if context.joker_main and G.GAME.current_round.hands_left == 0 and (to_big(card.ability.extra.mult) > to_big(1)) then --gives mult
-          return{
-            mult_mod = card.ability.extra.mult,
-            sound = 'hpfx_1up',
-            message = localize{type = 'variable', key = 'a_mult', vars = {card.ability.extra.mult}}
-          }
-        elseif context.end_of_round then --triggers at end of round
-            if context.main_eval and (to_big{card.ability.extra.mult, card.ability.extra.mult_gain} > to_big(1)) then
-                if not context.blueprint_card then 
-                    if card.ability.extra.c_rounds >= card.ability.extra.rounds then -- gains mult only if the required amount of rounds have passed
-                        card.ability.extra.mult = card.ability.extra.mult + card.ability.extra.mult_gain
-                        card.ability.extra.c_rounds = 0 --resets counter
-                        return { --custom message (for flavor <3)
-                            message = 'Yum!',
-                            sound = "hpfx_gulp",
-                            colour = G.C.MULT,
-                            card = card
-                        }
-                    else
-                        card.ability.extra.c_rounds = card.ability.extra.c_rounds + 1 --otherwise adds 1 to the counter
-                    end
-                end
-            end
-        end
+        return maryMain(card, context) or maryEnd(card, context)
     end
 }
-
 SMODS.Joker{ --Farmer/Cain
     key = 'farmer',
     loc_txt = {
@@ -142,7 +162,6 @@ SMODS.Joker{ --Farmer/Cain
         end
     end ]]
 }
-
 --[[ SMODS.Joker{ --Iscariot/Judas
     key = 'iscariot',
     config = {extra = {chips = 30, chip_gain = 3}},
@@ -213,7 +232,6 @@ SMODS.Joker{ --Farmer/Cain
         end
     end
 } ]]
-
 --[[ SMODS.Joker{ --Cyanosis/Blue Baby
     key = 'cyanosis',
     config = {
@@ -254,10 +272,8 @@ SMODS.Joker{ --Farmer/Cain
 } ]]
 
 --mult-decrease scoring context designed by BakersDozenBagels below!
-
 --[[ local last_mult = 0
 local raw_mod_mult = mod_mult ]]
-
 -- mod_mult is used after any mult change to apply effects like Rich Get Richer
 --[[ function mod_mult(...)
     local new_mult = raw_mod_mult(...)
@@ -267,8 +283,7 @@ local raw_mod_mult = mod_mult ]]
                 G.jokers.cards[i]:calculate_joker({
                     hyperfixation_mod_mult_decrease = last_mult - new_mult,
                 })
-            end
-            
+            end          
         end
     end
     last_mult = new_mult
@@ -276,7 +291,6 @@ local raw_mod_mult = mod_mult ]]
 end ]]
 
 -- Reset our tracker at the end of the round so we don't see a fake decrease
-
 --[[ local raw_G_FUNCS_evaluate_play = G.FUNCS.evaluate_play
 function G.FUNCS.evaluate_play(...)
     raw_G_FUNCS_evaluate_play(...)
@@ -291,3 +305,4 @@ Card.use_consumeable = function(self, area, copier)
   cuc(self, area, copier)
   if self.config.center.key == 'c_wheel' then G.GAME.probabilities.normal = G.GAME.probabilities.normal / odds_mod end
 end ]]
+
