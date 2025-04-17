80aacb3e01e054a4ef2fe867a6ab59509cdc4ca7
mimi! <72160950+tomatoseandcrying@users.noreply.github.com>
2025-04-17 19:03:40 -0400

    look at this one instead

--- Stickers.lua
+++ Stickers.lua
@@ -11,16 +11,22 @@ SMODS.Sticker{
     atlas = "hpfxstickers",
     pos = {x = 0, y = 0},
     badge_colour = HEX("FCB3EA"),
+    sets = {
+        Joker = true,
+        Consumable = false
+    },
     rate = 0,
     needs_enabled_flag = false,
     calculate = function(self, card, context)
+    end,
+    should_apply = function(self, card, center, area, bypass_roll)
+        return true
     end,
     apply = function(self, card, val)
         card.ability[self.key] = val
         card:set_cost()
     end
 }
-
 local nosellsticker_hook = Card.can_sell_card
 function Card:can_sell_card(context)
     if self.ability.hpfx_priceless then
