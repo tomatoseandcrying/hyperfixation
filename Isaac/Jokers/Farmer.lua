fbedfc15879688d72b4006af449a21b5bd73967b
mimi! <72160950+tomatoseandcrying@users.noreply.github.com>
2025-04-30 20:18:07 -0400

    split of the rest of the issacs (except cyan)

---
+++ Isaac/Jokers/Farmer.lua
@@ -0,0 +1,29 @@
+SMODS.Joker{
+    key = 'farmer',
+    loc_txt = {
+    },
+    config = {extra = {size = 1}},
+    unlocked = false,
+    discovered = false,
+    loc_vars = function(self, info_queue, card)
+        return {vars = {card.ability.extra.size}}
+    end,
+    rarity = 2,
+    atlas = 'IsaacJokers',
+    pos = {x = 4, y = 1},
+    cost = 5,
+    blueprint_compat = false,
+    eternal_compat = true,
+    perishable_compat = true,
+    add_to_deck = function(self, card, from_debuff)
+        card.ability.extra.size = math.floor(card.ability.extra.size)
+        G.jokers.config.card_limit = G.jokers.config.card_limit + card.ability.extra.size
+        G.hand.config.highlighted_limit = G.hand.config.highlighted_limit + card.ability.extra.size
+    end,
+    remove_from_deck = function(self, card, from_debuff)
+        G.jokers.config.card_limit = G.jokers.config.card_limit - card.ability.extra.size
+        G.hand.config.highlighted_limit = G.hand.config.highlighted_limit - card.ability.extra.size
+        if G.hand.config.highlighted_limit < 5 then G.hand.config.highlighted_limit = 5 end
+		G.hand:unhighlight_all()
+    end
+}
---
+++ Isaac/Jokers/Iscariot.lua
@@ -0,0 +1,47 @@
+SMODS.Joker{ --Iscariot/Judas
+    key = 'iscariot',
+    config = {extra = {chips = 30, chip_gain = 3}},
+    rarity = 2,
+    atlas = 'IsaacJokers',
+    pos = {x = 4, y = 1},
+    cost = 3,
+    unlocked = false,
+    discovered = false,
+    blueprint_compat = true,
+    eternal_compat = true,
+    perishable_compat = true,
+    ScoreReset = true,
+    loc_vars = function(self, info_queue, card)
+        return {vars = {card.ability.extra.chips, card.ability.extra.chip_gain}}
+    end,
+    calculate = function (self, card, context)
+        local blind_keys = {
+            bl_window = true,
+            bl_head = true,
+            bl_club = true,
+            bl_goad = true,
+            bl_plant = true,
+            bl_tooth = true,
+            bl_pillar = true,
+            bl_flint = true,
+            bl_eye = true,
+            bl_mouth = true,
+            bl_psychic = true,
+            bl_arm = true,
+            bl_ox = true,
+            bl_final_leaf = true
+        }
+        if context.joker_main and 
+        (to_big{card.ability.extra.chips, card.ability.extra.chip_gain} > to_big(1)) then
+            isaacChip(card, context) --chips scoring function
+        end
+        if context.debuffed_hand and blind_keys[G.GAME.blind.config.blind.key] then
+            card.ability.extra.chips = card.ability.extra.chips + card.ability.extra.chip_gain
+            return {
+                message = 'Silver!',
+                colour = G.C.CHIPS,
+                card = card
+            }
+        end
+    end
+}
