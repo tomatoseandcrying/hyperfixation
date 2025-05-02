120302c4464ff6b78c8145a926e7cae999a68243
mimi! <72160950+tomatoseandcrying@users.noreply.github.com>
2025-05-01 22:38:15 -0400

    oh and also moriah

--- Isaac/Jokers/Moriah.lua
+++ Isaac/Jokers/Moriah.lua
@@ -26,6 +26,9 @@ SMODS.Joker{
     blueprint_compat = true,
     eternal_compat = true,
     perishable_compat = true,
+    remove_from_deck = function (self, card, from_debuff)
+        play_sound((('hpfx_death') .. pseudorandom("isold", 1, 3)), 1, 0.55)
+    end,
     calculate = function(self, card, context)
         if context.joker_main and 
         (to_big(card.ability.extra.chips) > to_big(1)) and
