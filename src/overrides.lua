b5e64dfcd73d765498fb093a3f47acc171fbc713
jolyne <72160950+tomatoseandcrying@users.noreply.github.com>
2025-05-30 15:47:01 -0400

    Revert "walk it back"
    This reverts commit 2f03c6772a5b7cbbb23fdcc65ba1539f7528fa84.

--- src/overrides.lua
+++ src/overrides.lua
@@ -28,16 +28,6 @@ function SMODS.calculate_context(context, return_table)
         end
     end
     if context.using_consumeable and not context.blueprint_card and G.GAME.chudhit > 0 then
-        for _, card in ipairs(G.jokers.cards) do
-            if card.key == "j_hpfx_chud" then
-                card.ability.extra.xmult = card.ability.extra.xmult * card.ability.extra.xmult_gain
-                return {
-                    message = localize{type = 'variable', key = 'a_xmult', vars = {card.ability.extra.xmult}},
-                    colour = G.C.MULT,
-                    card = card,
-                }
-            end
-        end
     end
     return ccc(context, return_table)
 end
