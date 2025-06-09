a2d8be8280635a452e2c6f8554634ee2c23358be
jolyne <72160950+tomatoseandcrying@users.noreply.github.com>
2025-06-09 03:03:24 -0400

    minor balance patch

--- 4Fun/Jokers/Chud Joker.lua
+++ 4Fun/Jokers/Chud Joker.lua
@@ -23,7 +23,7 @@ SMODS.Joker{
     end,
     add_to_deck = function(self, card, from_debuff)
         for k, v in pairs(G.GAME.probabilities) do
-            G.GAME.probabilities[k] = v--/10
+            G.GAME.probabilities[k] = v/10
         end
     end,
     remove_from_deck = function(self, card, from_debuff)
