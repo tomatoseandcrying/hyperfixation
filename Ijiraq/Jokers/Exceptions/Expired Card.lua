29ad178aa77cc3fa624880038174c538984fa0c1
jolyne <72160950+tomatoseandcrying@users.noreply.github.com>
2025-06-12 00:01:18 -0400

    .

--- Ijiraq/Jokers/Exceptions/Expired Card.lua
+++ Ijiraq/Jokers/Exceptions/Expired Card.lua
@@ -1,20 +1,22 @@
 G.C.hpfx_IjiGray = HEX('BFD7D5')
-SMODS.Joker{ --Credit Card?
+SMODS.Joker { --Credit Card?
     key = 'expired',
-    pos = {x = 5, y = 1},
+    pos = { x = 5, y = 1 },
     no_mod_badges = true,
     unlocked = true,
     discovered = true,
     --no_collection = true,
     config = {
-        extra = {credit = 20, dollars = 0}
+        extra = { credit = 20, dollars = 0 }
     },
-    loc_vars = function (self, info_queue, card)
-        return{vars = {
-            card.ability.extra.credit,
-            card.area and card.area == G.jokers and "...?" or "",
-            card.ability.extra.dollars
-        }}
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = {
+                card.ability.extra.credit,
+                card.area and card.area == G.jokers and "...?" or "",
+                card.ability.extra.dollars
+            }
+        }
     end,
     generate_ui = function(self, info_queue, card, desc_nodes, specific_vars, full_UI_table)
         full_UI_table.name = localize { type = 'name', set = "Joker", key = card.ability and card.ability.extra.new_key or "j_hpfx_expired", nodes = {} }
@@ -31,24 +33,11 @@ SMODS.Joker{ --Credit Card?
     cost = 1,
     atlas = 'IjiraqJokers',
     blueprint_compat = false,
-    calculate = function (self, card, context)
-        local fuckYou = false
-        if context.buying_card and
-        not context.blueprint then
-            fuckYou = true
-        end
-        if to_big(G.GAME.dollars) <=
-        to_big(card.ability.extra.dollars) and
-        fuckYou then
-            G.E_MANAGER:add_event(Event({
-                trigger = "immediate",
-                delay = 0,
-                func = function()
-                hpfx_Transform(card, context)
-                return true
-                end,
-            }))
-            fuckYou = false
+    calculate = function(self, card, context)
+        if context.buying_card and not context.blueprint
+        and to_big(G.GAME.dollars) - context.card.cost <=
+        to_big(card.ability.extra.dollars) then
+            hpfx_Transform(card, context)
         end
     end
 }
