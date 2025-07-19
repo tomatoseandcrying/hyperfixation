b26678391180361814af819605b757c42077415b
jolyne <72160950+tomatoseandcrying@users.noreply.github.com>
2025-07-18 21:22:28 -0400

    ok NOW it's fixed

--- Ijiraq/Jokers/Exceptions/Dawn.lua
+++ Ijiraq/Jokers/Exceptions/Dawn.lua
@@ -14,7 +14,7 @@ SMODS.Joker {
         extra = {
             repetitions = 1,
             saved_cards = {},
-            has_logged = false
+            duplicated_cards = {}
         }
     },
     loc_vars = function(self, info_queue, card)
@@ -39,76 +39,58 @@ SMODS.Joker {
         local sticker = SMODS.Stickers['hpfx_priceless']
         sticker.apply(sticker, card, true)
         if not card.ability then
-            card.ability = { extra = { saved_cards = {}, has_logged = false } }
+            card.ability = { extra = { saved_cards = {}, duplicated_cards = {} } }
         elseif not card.ability.extra then
-            card.ability.extra = { saved_cards = {}, has_logged = false }
+            card.ability.extra = { saved_cards = {}, duplicated_cards = {} }
         end
     end,
     calculate = function(self, card, context)
-        if not card.ability then
-            card.ability = { extra = { saved_cards = {}, has_logged = false } }
-        elseif not card.ability.extra then
-            card.ability.extra = { saved_cards = {}, has_logged = false }
-        end
         local extra = card.ability.extra
-
         if context.before then
             extra.saved_cards = {}
+            extra.duplicated_cards = {}
             for _, v in ipairs(context.full_hand or {}) do
                 table.insert(extra.saved_cards, v)
             end
-            extra.has_logged = false
             return
         end
-
-        -- Only run duplication once per hand, after scoring
         if context.repetition and context.cardarea == G.play
-            and G.GAME.current_round.hands_left == 0 and not extra.has_logged then
-            extra.has_logged = true -- <--- Set this here to prevent double duplication
-
-            G.E_MANAGER:add_event(Event({
-                trigger = 'immediate',
-                delay = 0.01,
-                func = function()
-                    for _, v in ipairs(extra.saved_cards) do
-                        local _card = copy_card(v, nil, nil, G.playing_card)
-                        _card:add_to_deck()
-                        G.deck.config.card_limit = G.deck.config.card_limit + 1
-                        table.insert(G.playing_cards, _card)
-                        G.deck:emplace(_card)
-                        _card.states.visible = nil
-
-                        G.E_MANAGER:add_event(Event({
-                            func = function()
-                                _card:start_materialize()
-                                return true
-                            end
-                        }))
-                    end
-                    return true
-                end
-            }))
-            return {
-                repetitions = card.ability.extra.repetitions,
-                message = localize('k_copied_ex'),
-                colour = G.C.CHIPS,
-                func = function()
+            and G.GAME.current_round.hands_left == 0 then
+            return { repetitions = extra.repetitions }
+        end
+        if context.individual and context.cardarea == G.play and G.GAME.current_round.hands_left == 0 then
+            for _, saved in ipairs(extra.saved_cards) do
+                if saved == context.other_card and not extra.duplicated_cards[saved] then
+                    extra.duplicated_cards[saved] = true
                     G.E_MANAGER:add_event(Event({
+                        trigger = 'after',
+                        delay = 0.1,
                         func = function()
-                            SMODS.calculate_context({
-                                playing_card_added = true,
-                                cards = _card
-                            })
+                            local _card = copy_card(saved, nil, nil, G.playing_card)
+                            _card:add_to_deck()
+                            G.deck.config.card_limit = G.deck.config.card_limit + 1
+                            table.insert(G.playing_cards, _card)
+                            G.deck:emplace(_card)
+                            _card.states.visible = nil
+
+                            G.E_MANAGER:add_event(Event({
+                                func = function()
+                                    _card:start_materialize()
+                                    return true
+                                end
+                            }))
+                            SMODS.calculate_context({ playing_card_added = true, cards = _card })
                             hpfx_Transform(card, context)
                             return true
                         end
                     }))
+                    break
                 end
-            }
+            end
         end
         if context.end_of_round then
             extra.saved_cards = {}
-            extra.has_logged = false
+            extra.duplicated_cards = {}
             return
         end
     end
