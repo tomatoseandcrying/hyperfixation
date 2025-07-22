efd8e68828bea040882de8b1b7cd7653034184d0
jolyne <72160950+tomatoseandcrying@users.noreply.github.com>
2025-07-22 19:37:21 -0400

    michelle almost fixed! now for the eternal bug....

--- Ijiraq/Jokers/Exceptions/Nanners.lua
+++ Ijiraq/Jokers/Exceptions/Nanners.lua
@@ -40,32 +40,38 @@ SMODS.Joker {
         sticker.apply(sticker, card, true)
     end,
     calculate = function(self, card, context)
-        if context.check_eternal and not context.blueprint then
+        if context.check_eternal and not context.blueprint and card.config.center.key == 'j_hpfx_close_michelle' then
             return { no_destroy = { override_compat = true } }
         end
-        if context.end_of_round and context.game_over == false and
-            context.main_eval and not context.blueprint then
-            if SMODS.pseudorandom_probability(card, 'hpfx_michelle_seed', context.numerator, card.ability.extra.dynadenom, 'hpfx_michelle_id') then
+        if context.end_of_round and context.game_over == false and context.main_eval and not context.blueprint then
+            local numerator = tonumber(context.numerator) or 1
+            local dynadenom = card.ability.extra.dynadenom or 1
+
+            if SMODS.pseudorandom_probability(card, 'hpfx_michelle_seed', numerator, dynadenom, 'hpfx_michelle_id') then
                 G.E_MANAGER:add_event(Event({
                     func = function()
                         play_sound('tarot1')
-                        card.T.r = -0.2
                         card:juice_up(0.3, 0.4)
-                        card.states.drag.is = true
-                        card.children.center.pinch.x = true
-                        local michelleSacrifice = pseudorandom_element(G.jokers.cards, pseudoseed("hpfx_close_michelle"))
-                        G.E_MANAGER:add_event(Event({
-                            trigger = 'after',
-                            delay = 0.3,
-                            blockable = false,
-                            func = function()
-                                hpfx_Transform(michelleSacrifice, context)
-                                if card.ability.extra.dynadenom > 0 then
-                                    card.ability.extra.dynadenom = card.ability.dynadenom + 1
-                                end
-                                return true
+                        local candidates = {}
+                        for _, j in ipairs(G.jokers.cards) do
+                            if j ~= card then
+                                table.insert(candidates, j)
                             end
-                        }))
+                        end
+                        local michelleSacrifice = #candidates > 0 and
+                            pseudorandom_element(candidates, pseudoseed("hpfx_close_michelle")) or nil
+                        if michelleSacrifice then
+                            G.E_MANAGER:add_event(Event({
+                                trigger = 'after',
+                                delay = 0.3,
+                                blockable = false,
+                                func = function()
+                                    hpfx_Transform(michelleSacrifice, context)
+                                    card.ability.extra.dynadenom = dynadenom + 1
+                                    return true
+                                end
+                            }))
+                        end
                         return true
                     end
                 }))
@@ -75,6 +81,7 @@ SMODS.Joker {
             else
                 return {
                     message = localize('hpfx_wither_ex'),
+                    no_destroy = { override_compat = true },
                     func = function()
                         hpfx_Transform(card, context)
                     end
