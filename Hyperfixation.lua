2b68c53b830fa4bd344d140a36244b1e3d100470
jolyne <72160950+tomatoseandcrying@users.noreply.github.com>
2025-09-13 18:25:26 -0400

    0.5 TESTING PHASE IS OUT

--- Hyperfixation.lua
+++ Hyperfixation.lua
@@ -833,8 +833,11 @@ Card_Character.add_speech_bubble = function(self, arg1, arg2, arg3)
     end
 end
 
---Stored Debug Functions
---[[ function maxx_debug(txt)
+
+
+--list of debug functions
+
+function rendercheck(txt) --Test text printing here
     attention_text({
         text = txt,
         scale = 1.3,
@@ -845,23 +848,33 @@ end
         offset = { x = 0, y = (G.STATE == G.STATES.TAROT_PACK or G.STATE == G.STATES.SPECTRAL_PACK) and -0.2 or 0 },
         silent = true
     })
-end ]]
---[[ function porcelaintest()
+end
+
+function porctest() --Turns your entire deck into Stone cards
     for _, card in ipairs(G.playing_cards) do
         card:set_ability(G.P_CENTERS.m_stone)
     end
-end ]]
---[[ function tomaheart(count)
-    G.PROFILES[G.SETTINGS.profile].hpfx_crimsonCount = count
-end ]]
+end
 
---Active Debug Functions
-function guh()
+function cyanunlock(count) --Manually set the number of times Crimson Heart was defeated
+    G.PROFILES[G.SETTINGS.profile].hpfx_crimsonCount = count
+end
+
+function guh() --Convert the Joker you're hovering over into its Jokester variant
     local selected = G.CONTROLLER and
         (G.CONTROLLER.focused.target or G.CONTROLLER.hovering.target)
+    if Hyperglobal.brokejokes[selected.config.center.key] then return print('This one\'s disabled until I can fix it!') end
     G.GAME.current_round.fodder_card.jkey = selected.config.center.key
     if JokerDisplay then
         selected:joker_display_remove()
     end
     selected:set_ability('j_hpfx_costume')
 end
+
+debugs_one_line_long = {                         --other debug commands that just go into the console
+    "eval G.GAME.raqeffects",                    -- prints the current effects Ijiraq has stored
+    "eval Hyperglobal.exceptions",               -- prints the current table of Jokesters with custom transformation logic
+    "eval Hyperglobal.calcdollarjokesters",      -- prints the current table of Jokesters that calculate dollar bonuses
+    "eval G.jokers.cards[1].config.center.key",  -- checks the first joker's key
+    "eval G.GAME.current_round.fodder_card.jkey" -- checks the stored key of the fodder card
+}
