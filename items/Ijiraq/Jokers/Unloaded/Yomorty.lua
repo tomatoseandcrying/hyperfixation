6129ec3e650c483081f7b951c3629d330151fe04
jolyne <72160950+tomatoseandcrying@users.noreply.github.com>
2025-09-19 22:29:47 -0400

    yorick? scale card

--- items/Ijiraq/Jokers/Unloaded/Yomorty.lua
+++ items/Ijiraq/Jokers/Unloaded/Yomorty.lua
@@ -53,10 +53,18 @@ SMODS.Joker {
                     if will_trigger then
                         local overflow = subtract - dis_rem
                         card.ability.extra.dis_rem = card.ability.extra.dis
-                        card.ability.extra.xmult =
-                            card.ability.extra.xmult + card.ability.extra.xmult_gain
                         card.ability.extra.dis_rem =
                             card.ability.extra.dis_rem - overflow
+                        SMODS.scale_card(card, {
+                            operation = "+",
+                            ref_table = card.ability.extra,
+                            ref_value = "xmult",
+                            scalar_value = "xmult_gain",
+                            scaling_message = {
+                                message = 'Something!?',
+                                colour = G.C.MULT
+                            }
+                        })
                     else
                         card.ability.extra.dis_rem = dis_rem - subtract
                     end
