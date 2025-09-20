2a593037c34cb65692102c49e12c523a1a27b65e
jolyne <72160950+tomatoseandcrying@users.noreply.github.com>
2025-09-19 22:59:27 -0400

    everyone knows im a bad bitch

--- items/4Fun/Jokers/No Bitches.lua
+++ items/4Fun/Jokers/No Bitches.lua
@@ -11,34 +11,54 @@ SMODS.Joker {
     eternal_compat = true,
     perishable_compat = true,
     config = {
+        qc = 0,
+        changes = 1,
         extra = {
             xmult_loss = 1,
             xmult = 4,
         }
     },
     loc_vars = function(self, info_queue, card)
-        local qc = 0
         if G.playing_cards then
             for _, playing_card in ipairs(G.playing_cards) do
-                if playing_card:get_id() == 12 then qc = qc + 1 end
+                if playing_card:get_id() == 12 then
+                    SMODS.scale_card(card, {
+                        ref_table = card.ability,
+                        ref_value = 'qc',
+                        scalar_value = 'changes',
+                        operation = '+',
+                        block_overrides = {
+                            message = true
+                        }
+                    })
+                end
             end
         end
-        if card.ability.extra.xmult - qc < 0 then
+        if card.ability.extra.xmult - card.ability.qc < 0 then
             card.ability.extra.xmult = 0
         end
-        return { vars = { card.ability.extra.xmult_loss, math.max(card.ability.extra.xmult - qc, 0) } }
+        return { vars = { card.ability.extra.xmult_loss, math.max(card.ability.extra.xmult - card.ability.qc, 0) } }
     end,
     calculate = function(self, card, context)
         if context.joker_main then
-            local qc = 0
             if G.playing_cards then
                 for _, playing_card in ipairs(G.playing_cards) do
-                    if playing_card:get_id() == 12 then qc = qc + 1 end
+                    if playing_card:get_id() == 12 then
+                        SMODS.scale_card(card, {
+                            ref_table = card.ability,
+                            ref_value = 'qc',
+                            scalar_value = 'changes',
+                            operation = '+',
+                            block_overrides = {
+                                message = true
+                            }
+                        })
+                    end
                 end
             end
-            if math.max(card.ability.extra.xmult - qc, 0) > 0 then
+            if math.max(card.ability.extra.xmult - card.ability.qc, 0) > 0 then
                 return {
-                    xmult = card.ability.extra.xmult - qc
+                    xmult = card.ability.extra.xmult - card.ability.qc
                 }
             else
                 play_sound("hpfx_vineboom")
