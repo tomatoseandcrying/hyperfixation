9235709470ae8f7032caf06b4740fae6d51ac095
jolyne <72160950+tomatoseandcrying@users.noreply.github.com>
2025-10-01 18:09:45 -0400

    moriah jdisp

--- lib/joker-display_defs.lua
+++ lib/joker-display_defs.lua
@@ -1,31 +1,30 @@
 local jd_def = JokerDisplay.Definitions
+---@type JDJokerDefinition
 jd_def["j_hpfx_moriah"] = {
     text = {
         { text = "+" },
-        { ref_table = "card.ability.extra", ref_value = "chips", retrigger_type },
+        { ref_table = "card.ability.extra", ref_value = "chips", retrigger_type = "mult" },
     },
     text_config = {
-        scale = 0.35,
-        colour = G.C.CHIPS
+        colour = G.C.CHIPS,
+        scale = 0.4
     },
     extra = {
         {
             { text = "+" },
-            { ref_table = "card.ability.extra", ref_value = "mult" }
+            { ref_table = "card.ability.extra", ref_value = "mult", retrigger_type = "mult" },
         },
     },
     extra_config = {
-        scale = 0.35,
-        colour = G.C.MULT
+        colour = G.C.MULT,
+        scale = 0.35
     },
     reminder_text = {
-        {
-            { text = "$" },
-            { ref_table = "card.ability.extra", ref_value = "money" }
-        },
+        { text = "+$" },
+        { ref_table = "card.ability.extra", ref_value = "money", retrigger_type = "mult" },
     },
     reminder_text_config = {
-        scale = 0.35,
-        colour = G.C.MONEY
+        colour = G.C.MONEY,
+        scale = 0.3
     },
 }
