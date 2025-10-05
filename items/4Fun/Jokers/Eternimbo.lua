b29b2a7cb903d82659d04fe36182d162fadecd48
jolyne <72160950+tomatoseandcrying@users.noreply.github.com>
2025-10-04 20:13:03 -0400

    i forgot to take old man out of  the pool

--- items/4Fun/Jokers/Eternimbo.lua
+++ items/4Fun/Jokers/Eternimbo.lua
@@ -1,6 +1,9 @@
 SMODS.Joker {
-    key = 'eternimbo',
-    atlas = 'Jokers4Fun',
+    key           = 'eternimbo',
+    atlas         = 'Jokers4Fun',
     no_collection = true,
-    pos = { x = 4, y = 0 }
+    pos           = { x = 4, y = 0 },
+    in_pool       = function(self, args)
+        return false
+    end
 }
