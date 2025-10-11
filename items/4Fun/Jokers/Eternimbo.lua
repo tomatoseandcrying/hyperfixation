a2d08f72b2aa94a67661ab11991befb326c107d0
jolyne <72160950+tomatoseandcrying@users.noreply.github.com>
2025-10-10 20:05:13 -0400

    credit card? fixed, gonna make 0.6.1

--- items/4Fun/Jokers/Eternimbo.lua
+++ items/4Fun/Jokers/Eternimbo.lua
@@ -1,9 +1,7 @@
 SMODS.Joker {
-    key           = 'eternimbo',
-    atlas         = 'Jokers4Fun',
+    key = 'eternimbo',
+    atlas = 'Jokers4Fun',
     no_collection = true,
-    pos           = { x = 4, y = 0 },
-    in_pool       = function(self, args)
-        return false
-    end
+    pos = { x = 4, y = 0 },
+    in_pool = function(self, args) return false end
 }
