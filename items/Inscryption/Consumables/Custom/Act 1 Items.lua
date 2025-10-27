fefc9eb4ba0aec76b56b1d8bdc820da62d999da4
jolyne <72160950+tomatoseandcrying@users.noreply.github.com>
2025-10-27 10:44:07 -0400

    renaming

--- items/Inscryption/Consumables/Custom/Act 1 Items.lua
+++ items/Inscryption/Consumables/Custom/Act 1 Items.lua
@@ -2,4 +2,7 @@ SMODS.ConsumableType({
     key = 'hpfx_inscr_act1_items',
     primary_colour = HEX('F97717'),
     secondary_colour = G.C.BLACK,
+    loc_txt = {
+        name = 'Inscryption Act 1',
+    },
 })
