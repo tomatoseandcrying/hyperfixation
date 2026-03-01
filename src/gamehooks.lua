dd7d109dc503a4d615281b2e789dc2cc629f9ae7
jolyne <72160950+tomatoseandcrying@users.noreply.github.com>
2026-03-01 16:41:09 -0500

    plug of doom has been eradicated

--- src/gamehooks.lua
+++ src/gamehooks.lua
@@ -185,7 +185,6 @@ Game.main_menu = function(change_context)
         function G.SPLASH_HPFX_LOGO:click()
             play_sound('button', 1, 0.3)
             play_sound('hpfx_faaaah', 1, 0.8)
-            G.FUNCS.Incognito_Plug()
         end
 
         function G.SPLASH_HPFX_LOGO:hover()
